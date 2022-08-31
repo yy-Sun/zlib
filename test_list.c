251c8eb67e3a64710e93cbde6f5e5d763ec15f37
zhuyy <2485480126@qq.com>
2022-08-31 23:22:38 +0800

    gtest: add support in cmake for gtest

--- test_list.c
+++ test_list.c
@@ -1,17 +1,9 @@
-#include <stdlib.h>
-#include "gtest/gtest.h"
-#include "zlist.h"
-int add(int a, int b){
-    return a + b;
-}
+#include <gtest/gtest.h>
 
-TEST(List, init){
-    EXPECT_EQ(3, add(1, 2));
-}
-
-int 
-main(int argc, char **argv){
-    testing::InitGoogleTest(&argc, argv)
-    return RUN_ALL_TESTS();
-
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+// Expect two strings not to be equal.
+EXPECT_STRNE("hello", "world");
+// Expect equality.
+EXPECT_EQ(7 * 6, 42);
 }
